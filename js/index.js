1995d6f4f4c578bd0f9c22a1f22619c9c06d2bcd
DerStorm <stas.khristich@mail.ru>
2021-06-14 20:47:12 +0300

    Added a database and in index.js leaves a request to output db fields

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.news');
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:3000/posts';
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+
+    let template = '';
+    posts.forEach(post => {
+        template += `
+        <div class="post">
+          <h2>${post.title}</h2>
+          <p><small>${post.likes} likes</small></p>
+          <p>${post.body.slice(0, 200)}...</p>
+          <a href="/newsDetails.html?id=${post.id}">Read more</a>
+        </div>
+      `
+    })
+    container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
